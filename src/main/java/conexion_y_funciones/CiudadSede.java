058b77871bc373c67e344330d90d76973bb6e04c
Carlos Edward Grajales M <cgrajales@colombiasoftware.net>
2022-02-12 22:45:08 -0500

    adcionando mapeo de base de datos

--- src/main/java/conexion_y_funciones/CiudadSede.java
+++ src/main/java/conexion_y_funciones/CiudadSede.java
@@ -1,5 +1,5 @@
 package conexion_y_funciones;
-// Generated 26/01/2022 11:28:34 PM by Hibernate Tools 4.3.1
+// Generated 12/02/2022 10:43:28 PM by Hibernate Tools 4.3.1
 
 
 import java.util.HashSet;
