5e8885ef44195e5382ae410db1eda6933971d2ad
W-80 <82257654+W-80@users.noreply.github.com>
2022-02-16 05:54:14 -0500

    Permisos de rol

--- src/main/java/com/mycompany/flash_project/Main.java
+++ src/main/java/com/mycompany/flash_project/Main.java
@@ -5,7 +5,7 @@
  */
 package com.mycompany.flash_project;
 
-
+import java.util.*;
 import conexion_y_funciones.*;
 import interfaz.JFrame_inicio;
 import interfaz.Factura;
@@ -47,36 +47,73 @@ public class Main {
      */
     public static void main(String[] args) throws SQLException, URISyntaxException {
         new JFrame_principal().setVisible(true);
-
+        
+        //Funciones2 funcion = new Funciones2();
+        //ArrayList<RolEmpleados> A=funcion.listaRoles();
+        //System.out.println(A.get(0).getTipoEmpleado());
+       
+        //InformacionEmpleados a=funcion.iniciosesion("ESPARTAN1841@HOTMAIL.ES","pared14");
+        
+        //System.out.println(a.getDocumentoEmpleado());
+       
+        
+    
 
         //new JFrame_operador().setVisible(true);
         //new JFrame_secretaria().setVisible(true);
         
-        
+        /*
         SessionFactory factory = HibernateUtil.buildSessionFactory();
         Session session = factory.openSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction(); 
             RolEmpleados rol;
+        
+            rol=new RolEmpleados();
+            rol.setIdTipoEmpleado(1);
+            rol.setTipoEmpleado("Gerente");
+            session.saveOrUpdate(rol);
             
+            rol=new RolEmpleados();
+            rol.setIdTipoEmpleado(2);
+            rol.setTipoEmpleado("Secretario");
+            session.saveOrUpdate(rol);
+            
+            rol=new RolEmpleados();
+            rol.setIdTipoEmpleado(3);
+            rol.setTipoEmpleado("Contador");
+            session.saveOrUpdate(rol);
+            /*
+            rol = new PermisosRol();
+            rol.setCrearSede(1);
+            rol.setConsultarSede(1);
+            rol.setEliminarSede(1);
+            rol.setCrearUsuario(1);
+            rol.setEditarUsuario(1);
+            rol.setEliminarUsuario(1);
+            rol.setTerminalVentas(1);
+            session.saveOrUpdate(rol);
+        */
+           
+        
+           /*
             for(int i=1;i<=10;i++){
                 rol=new RolEmpleados();
                 rol.setIdTipoEmpleado(i);
                 rol.setTipoEmpleado("ROL8 "+i);
                 session.saveOrUpdate(rol);    
             }
-            
-            
-             Query query = session.createQuery("FROM RolEmpleados");
-            Iterator<RolEmpleados> it = query.iterate();
+          
+             //Query query = session.createQuery("FROM RolEmpleados");
+            //Iterator<RolEmpleados> it = query.iterate();
  
-            RolEmpleados R;
-            while (it.hasNext()) {
-                R = it.next();
-                System.out.println(R.getIdTipoEmpleado() + " " + R.getTipoEmpleado());
-            }
- 
+           // RolEmpleados R;
+            //while (it.hasNext()) {
+             //   R = it.next();
+             //   System.out.println(R.getIdTipoEmpleado() + " " + R.getTipoEmpleado());
+           // }
+
             tx.commit();
             
         } catch (HibernateException e) {
@@ -86,10 +123,10 @@ public class Main {
         } finally {
             session.close(); 
         }
-        
-        
+       */
+       
   
     }
+}
 
     
-}
--- src/main/java/interfaz/JFrame_principal.java
+++ src/main/java/interfaz/JFrame_principal.java
@@ -6,6 +6,7 @@
 package interfaz;
 
 import interfaz.InterfacesGestionSede.GestionSede;
+import interfaz.InterfacesGestionRol.GestionRoles;
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -20,6 +21,7 @@ public class JFrame_principal extends javax.swing.JFrame {
     GestionUsuario gestionUsuario = new GestionUsuario();
     GestionSede gestionSede = new GestionSede();
     Reportes reporte = new Reportes();
+    GestionRoles gestionRol = new GestionRoles();
     public JFrame_principal() { 
         initComponents();
         this.setLocationRelativeTo(null);
@@ -45,6 +47,7 @@ public class JFrame_principal extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
         jPanel7 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -93,30 +96,47 @@ public class JFrame_principal extends javax.swing.JFrame {
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/META-INF/logo2.png"))); // NOI18N
 
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("ADMINISTRAR ROLES");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
-            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
+            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2)
                 .addGap(65, 65, 65))
+            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(jLabel2)
-                .addGap(12, 12, 12)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(352, Short.MAX_VALUE))
+                .addGap(45, 45, 45)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(1, 1, 1)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(1, 1, 1)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(274, Short.MAX_VALUE))
         );
 
         jPanel7.setBackground(new java.awt.Color(254, 254, 254));
@@ -133,7 +153,7 @@ public class JFrame_principal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 605, Short.MAX_VALUE)
             .addComponent(jPanel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
@@ -147,6 +167,9 @@ public class JFrame_principal extends javax.swing.JFrame {
         
         reporte.setVisible(false);
         jPanel7.remove(reporte);
+        
+        gestionRol.setVisible(false);
+        jPanel7.remove(gestionRol);
         
         jPanel7.add(gestionUsuario);
         gestionUsuario.setVisible(true);
@@ -166,6 +189,9 @@ public class JFrame_principal extends javax.swing.JFrame {
         reporte.setVisible(false);
         jPanel7.remove(reporte);
         
+        gestionRol.setVisible(false);
+        jPanel7.remove(gestionRol);
+        
         jPanel7.add(gestionSede);
         gestionSede.setVisible(true);
     }//GEN-LAST:event_jButton3MouseClicked
@@ -178,9 +204,31 @@ public class JFrame_principal extends javax.swing.JFrame {
         gestionSede.setVisible(false);
         jPanel7.remove(gestionSede);
         
+        gestionRol.setVisible(false);
+        jPanel7.remove(gestionRol);
+        
         jPanel7.add(reporte);
         reporte.setVisible(true);
     }//GEN-LAST:event_jButton4MouseClicked
+
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        gestionUsuario.setVisible(false);
+        jPanel7.remove(gestionUsuario);
+        
+        gestionSede.setVisible(false);
+        jPanel7.remove(gestionSede);
+        
+        reporte.setVisible(false);
+        jPanel7.remove(reporte);
+        
+        jPanel7.add(gestionRol);
+        gestionRol.setVisible(true);        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2MouseClicked
 
     /**
      * @param args the command line arguments
@@ -221,6 +269,7 @@ public class JFrame_principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel2;
